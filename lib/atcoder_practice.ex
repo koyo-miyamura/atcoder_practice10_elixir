a024823657147ebb944c4b90b6abceed14325384
koyo <nexus9071@gmail.com>
2018-12-05 17:58:25 +0900

    4,5問

--- lib/atcoder_practice.ex
+++ lib/atcoder_practice.ex
@@ -75,4 +75,50 @@ defmodule AtcoderPractice do
       count
     end
   end
+
+  @doc """
+    ## Examples
+
+      iex> AtcoderPractice.coins(2,2,2,100)
+      2
+
+      iex> AtcoderPractice.coins(5,1,0,150)
+      0
+
+      iex> AtcoderPractice.coins(30,40,50,6000)
+      213
+  """
+  def coins(n500, n100, n50, total) do
+    for i <- 0..n50,
+        j <- 0..n100,
+        k <- 0..n500 do
+      i * 50 + j * 100 + k * 500
+    end
+    |> Enum.count(&(&1 == total))
+  end
+
+  @doc """
+    ## Examples
+
+      iex> AtcoderPractice.some_sums(20,2,5)
+      84
+
+      iex> AtcoderPractice.some_sums(10,1,2)
+      13
+
+      iex> AtcoderPractice.some_sums(100,4,16)
+      4554
+  """
+  def some_sums(n, a, b) do
+    1..n
+    |> Enum.filter(&(a <= digit_sum(&1) and digit_sum(&1) <= b))
+    |> Enum.sum()
+  end
+
+  defp digit_sum(n) do
+    Integer.to_string(n)
+    |> String.codepoints()
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
+  end
 end
